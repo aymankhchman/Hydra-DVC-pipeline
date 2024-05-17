d18664907929138099cf9bc831ae4d989cf2ce29
aymankhchman <54699812+aymankhchman@users.noreply.github.com>
2024-05-17 11:56:01 -0400

    Update pipeline.py
    Cleaning script

--- stages/pipeline.py
+++ stages/pipeline.py
@@ -16,15 +16,3 @@ def create_pipeline(config: DictConfig) -> Pipeline:
             pipeline_step = (step_name, hydra.utils.instantiate(estimator, _convert_="partial", _recursive_=False))
             steps.append(pipeline_step)
     return Pipeline(steps)
-
-
-if __name__ == "__main__":
-
-    create_pipeline()
-
-    import pandas as pd
-    df = pd.read_parquet("./dataset/2024/yellow_tripdata_2024_new_t.parquet")
-    y = df["tip_amount"]
-
-    # X_train_tr = check_pipeline.fit_transform(df)
-
